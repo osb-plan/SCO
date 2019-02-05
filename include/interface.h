7145405d5cbf4f7a51441abb5e1b9c09c7795c2c
osb-plan <osbplan@gmail.com>
2019-02-06 00:13:51 +0100

    Added extern to use in c++

--- include/interface.h
+++ include/interface.h
@@ -1,9 +1,13 @@
 #ifndef INTERFACE_H_
 #define INTERFACE_H_
-
+#ifdef __cplusplus
+extern "C" { 
+#endif
     void _printc(char *stream,
                  char * msg,
                  char * color);
     
-
+#ifdef __cplusplus
+}  
+#endif
 #endif
