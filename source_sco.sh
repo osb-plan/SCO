b31dca8ccd9ad04faa0e09ae132be4e4ff379c58
OsbPlan <osbplan@gmail.com>
2020-03-11 10:19:03 +0100

    BUGFIX: if LD_LIBRARY_PATH is empty a spourious ':' character is inserted at the begin of the variable

--- source_sco.sh
+++ source_sco.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 echo "Add libsco to PATH"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+STR=$(echo $LD_LIBRARY_PATH)
+
+if [ -z "$STR" ] 
+then
+	export LD_LIBRARY_PATH=$(pwd)
+else
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+fi
+
+echo $STR
+ 
